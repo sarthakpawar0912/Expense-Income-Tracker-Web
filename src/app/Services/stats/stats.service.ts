69157e805bfc3727262a0535f7fca2bf56bd678b
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-03 00:45:12 +0530

    Updated features and fixed issues

--- src/app/Services/stats/stats.service.ts
+++ src/app/Services/stats/stats.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = 'http://localhost:8080/';
+const BASIC_URL = "http://localhost:8080/";
+
 @Injectable({
   providedIn: 'root'
 })
